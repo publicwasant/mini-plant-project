8881f4cecf5498114230529b9318ba2d5fdb1a5e
61011212075 <54496578+61011212075@users.noreply.github.com>
2021-03-28 07:26:53 +0700

    order-basket
    update

--- lib/views/pages/basket/basket-page.dart
+++ lib/views/pages/basket/basket-page.dart
@@ -1,36 +1,143 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:mp_v1_0/controllers/button/button-tile.dart';
 
 class BasketPage extends StatefulWidget {
   dynamic data;
 
-  BasketPage ({dynamic data}) {
+  BasketPage({dynamic data}) {
     this.data = data;
   }
 
   @override
-  State <StatefulWidget> createState() {
+  State<StatefulWidget> createState() {
     return _BasketPageState(data: this.data);
   }
 }
 
 class _BasketPageState extends State<BasketPage> {
   dynamic data;
-  
-  _BasketPageState ({dynamic data}) {
+
+  _BasketPageState({dynamic data}) {
     this.data = data;
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar: new AppBar(
-        title: Text('ตะกร้าสินค้า'),
-        automaticallyImplyLeading: false,
+        body: DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          // backgroundColor: AppBarBackgroundColor,
+          title: Text('สั่งซื้อ'),
+          bottom: TabBar(
+            tabs: [
+              Tab(text: 'ตะกร้า', icon: Icon(Icons.scatter_plot)),
+              Tab(text: 'สั่งซื้อ', icon: Icon(Icons.spa)),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            basket(),
+            order(),
+          ],
+        ),
       ),
-      body: Center(
-        child: Text('basket-page.dart')
+    ));
+  }
+}
+
+class basket extends StatefulWidget {
+  @override
+  _basketState createState() => _basketState();
+}
+
+class _basketState extends State<basket> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.fromLTRB(30, 2, 30, 2),
+              child: Card(
+                clipBehavior: Clip.antiAlias,
+                child: Column(children: [
+                  ButtonTile(
+                          icon: Icons.image,
+                          title: 'ต้นตะบองเพรช',
+                          subTitle: 'ประเภท : สินค้าสำเร็จรูป',
+                          onTap: () async {})
+                      .build()
+                ]),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.fromLTRB(30, 2, 30, 2),
+              child: Card(
+                clipBehavior: Clip.antiAlias,
+                child: Column(children: [
+                  ButtonTile(
+                          icon: Icons.image,
+                          title: 'รูปภาพเพิ่มเติม',
+                          subTitle: '(แตะเพื่อเลือกรูปภาพ)',
+                          onTap: () async {})
+                      .build()
+                ]),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class order extends StatefulWidget {
+  @override
+  _orderState createState() => _orderState();
+}
+
+class _orderState extends State<order> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.fromLTRB(30, 2, 30, 2),
+              child: Card(
+                clipBehavior: Clip.antiAlias,
+                child: Column(children: [
+                  ButtonTile(
+                          icon: Icons.image,
+                          title: '19/03/2562',
+                          subTitle: 'สถานะ : ยืนยันการสั่งซื้อ',
+                          onTap: () async {})
+                      .build()
+                ]),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.fromLTRB(30, 2, 30, 2),
+              child: Card(
+                clipBehavior: Clip.antiAlias,
+                child: Column(children: [
+                  ButtonTile(
+                          icon: Icons.image,
+                          title: 'รูปภาพเพิ่มเติม',
+                          subTitle: '(แตะเพื่อเลือกรูปภาพ)',
+                          onTap: () async {})
+                      .build()
+                ]),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
